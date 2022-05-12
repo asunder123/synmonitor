672a1208f66a54608f1841a41d72195e6bbce810
asunder123 <anand.sunder@capgemini.com>
2022-05-13 00:36:52 +0530

    Config file based syn monitor

--- synmonitor.py
+++ synmonitor.py
@@ -3,31 +3,52 @@ from flask import Flask
 from datetime import datetime
 import requests
 import time
+import json 
+from configparser import ConfigParser
 
 app = Flask(__name__)
 
-val = input("Enter url")
-print(val)
+c=ConfigParser()
+
+#Read from config file
+read = c.read('config.ini')
+print("Read from config file",read)
+
+#Get endpoint from ini file
+url = c.get('endpoint','url')
+print("End point is ",url)
+
+#Fetch no of hits
+hits = c.get('endpoint','hits')
+print("Set no of hits to ...",hits)
 
 
-@app.route("/app")
+#sleep time
+pollperiod = c.get('endpoint','pollperiod')
+print("Configured poll period is....", pollperiod)
+
+
+@app.route("/syn")
 def web():
     now = datetime.now()
     current_time=now.strftime("%H:%M:%S")
-    resp=requests.get(val,verify=True)
+    resp=requests.get(url,verify=True)
     var=resp.text
     status=resp.status_code
-    print("Response:::",var)
+    #print("Response:::",var)
     i=0
     d=list()
-    for i in range(100):
+    for i in range(int(hits)):
         list_entries=str(i)+'::'+current_time+'::'+'RespCode:'+str(status)
         d.append(list_entries)
-        print("Entries:",d)
-        time.sleep(1)
+        print("Entries:",d,"Type:",type(d))
+        print("Components:\n",d[i].split("::"))
+        time.sleep(float(pollperiod))
         k=str(d)
-        if i>100:
+        if i>int(hits):
             break
-    return k
+        json_data=json.dumps(d)
+     
+    return json_data
 
 
